91dd8d5a5c3516d1862d174fcdda1f8500901951
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-17 13:01:13 -0700

    Add new instance of TicTacToe for testing

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -8,6 +8,8 @@ describe TicTacToe do
   describe '#winner?' do
     let(:board) { GameBoard.new }
     let(:possibilities) { Possibilities.new }
+    subject(:new_game) { described_class.new('p1', 'p2', board, possibilities) }
+
     context 'when player performs a winning move'
 
     it 'returns true when board reads X X X across top row' do
