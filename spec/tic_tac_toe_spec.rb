4c3c3bfddb4a4baa000723349d24dd5827601aa8
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-17 10:21:40 -0700

    Describe test for horizontal winning move

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -5,5 +5,8 @@ require_relative '../lib/tic_tac_toe'
 describe TicTacToe do
   describe '#winner?' do
     context 'when player performs a winning move'
+
+    it 'returns true when board reads X X X across top row' do
+    end
   end
 end
