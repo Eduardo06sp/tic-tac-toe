3f77b899e4ac3d82b740ce7ec39796a3e48724be
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-17 15:29:13 -0700

    Create method stubs to allow testing of win logic in #play_rounds

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -14,6 +14,13 @@ describe TicTacToe do
     subject(:new_game) { described_class.new(player1, player2, board, possibilities) }
 
     context 'when player performs a winning move'
+    before do
+      allow(new_game).to receive(:puts)
+      allow(new_game).to receive(:gets).and_return('p1')
+      allow(board).to receive(:space_empty?).and_return(true)
+      allow(board).to receive(:update)
+      allow(possibilities).to receive(:update)
+    end
 
     it 'returns true when board reads X X X across top row' do
     end
