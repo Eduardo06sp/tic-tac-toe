cf199d1001ad409aa690450dd2c6e27c162f7378
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-17 13:00:01 -0700

    Require and use GameBoard and Possibilities classes

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 require_relative '../lib/tic_tac_toe'
+require_relative '../lib/game_board'
+require_relative '../lib/possibilities'
 
 describe TicTacToe do
   describe '#winner?' do
+    let(:board) { GameBoard.new }
+    let(:possibilities) { Possibilities.new }
     context 'when player performs a winning move'
 
     it 'returns true when board reads X X X across top row' do
