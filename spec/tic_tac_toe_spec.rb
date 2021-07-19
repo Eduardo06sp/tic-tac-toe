076458087ea4eaeff7c535c574a741085986f4a1
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2021-07-18 20:16:33 -0700

    Move method stubs and before hook to beginning of test file

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -6,21 +6,22 @@ require_relative '../possibilities'
 require_relative '../player'
 
 describe TicTacToe do
-  describe '#winner?' do
-    let(:board) { GameBoard.new }
-    let(:possibilities) { Possibilities.new }
-    let(:player1) { Player.new('p1', 'X') }
-    let(:player2) { Player.new('p2', 'O') }
-    subject(:new_game) { described_class.new(player1, player2, board, possibilities) }
+  let(:board) { GameBoard.new }
+  let(:possibilities) { Possibilities.new }
+  let(:player1) { Player.new('p1', 'X') }
+  let(:player2) { Player.new('p2', 'O') }
+  subject(:new_game) { described_class.new(player1, player2, board, possibilities) }
 
+  before do
+    allow(new_game).to receive(:puts)
+    allow(new_game).to receive(:gets).and_return('p1')
+    allow(board).to receive(:space_empty?).and_return(true)
+    allow(board).to receive(:update)
+    allow(possibilities).to receive(:update)
+  end
+
+  describe '#winner?' do
     context 'when player performs a winning move'
-    before do
-      allow(new_game).to receive(:puts)
-      allow(new_game).to receive(:gets).and_return('p1')
-      allow(board).to receive(:space_empty?).and_return(true)
-      allow(board).to receive(:update)
-      allow(possibilities).to receive(:update)
-    end
 
     it 'returns true when board reads X X X across top row' do
       horizontal_win = { A1: 'X', B1: 'X', C1: 'X',
